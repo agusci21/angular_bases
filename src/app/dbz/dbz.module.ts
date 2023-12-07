6c08cd1080ccb27aec8bed5b723efe964764d228
Franco Agustin Cola Ibañez <agustincola54@gmail.com>
2023-12-07 17:25:19 -0300

    Add components

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './pages/main-page.component';
+import { ListComponent } from './components/dbz/components/list/list.component';
+import { AddCharacterComponent } from './components/dbz/components/add-character/add-character.component';
 
 @NgModule({
   declarations: [
     MainPageComponent,
+    ListComponent,
+    AddCharacterComponent,
   ],
   imports: [
     CommonModule,
